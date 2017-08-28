e2ee9443b33f50c24bc7af111f6e678d8dd2d58c
Rick Myers <slipdizzy@gmail.com>
2017-08-27 21:42:51 -0400

    Finished Chapter 1!

--- app/src/main/java/com/myerstyme/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/myerstyme/android/geoquiz/QuizActivity.java
@@ -2,12 +2,32 @@ package com.myerstyme.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private TrueButtonListener tbl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+        //just testing listeners
+        tbl = new TrueButtonListener(QuizActivity.this);
+        mTrueButton.setOnClickListener(tbl);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
