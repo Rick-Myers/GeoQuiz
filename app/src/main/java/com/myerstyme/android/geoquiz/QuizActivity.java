21113c6c9af4323ac7a2866fd6f3aefafb5f9be3
Rick Myers <slipdizzy@gmail.com>
2017-08-29 12:23:06 -0400

    Added logging to QuizActivity

--- app/src/main/java/com/myerstyme/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/myerstyme/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.myerstyme.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,6 +14,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mFalseButton;
     private Button mNextButton;
     private TextView mQuestionTextView;
+    private final String TAG = "QuizActivity";
 
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_oceans, true),
@@ -77,5 +79,29 @@ public class QuizActivity extends AppCompatActivity {
 
         updateQuestion();
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
     }
 }
